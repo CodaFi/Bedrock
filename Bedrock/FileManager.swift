33dfd1c9862c79cb5fa2ffe0cd758f1044a532d1
Robert Widmann <devteam.codafi@gmail.com>
2015-06-15 02:27:11 -0600

    Do directory stuff too

--- Bedrock/FileManager.swift
+++ Bedrock/FileManager.swift
@@ -9,25 +9,25 @@
 import Darwin.POSIX.sys
 
 struct FileManager {
-	static func fileExists(path: String) -> Bool {
+	static func fileExists(path: String) -> (fileExists: Bool, isDirectory: Bool) {
 		/// Path length, even after symbolic link resolution, must never exceed PATH_MAX.
 		let p = path.substringToIndex(advance(path.startIndex, Int(PATH_MAX))).fileSystemRepresentation()
 		var st = stat()
 		// Check for a symbolic link first.
 		if lstat(p, &st) < 0 {
-			return false
+			return (false, false)
 		}
 
 		// Don't touch symlinks.  They will expand and do terrible things sometimes.
 		if st.st_mode == S_IFLNK {
-			return true
+			return (true, false)
 		}
 
 		// Stat a real path.
 		if stat(p, &st) < 0 {
-			return false
+			return (false, false)
 		}
 
-		return true
+		return (true, st.st_mode == S_IFDIR)
 	}
 }
