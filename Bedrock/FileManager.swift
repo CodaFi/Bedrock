5bf8954ac1d3521cf7ef708e8264f7411a221b59
Robert Widmann <devteam.codafi@gmail.com>
2015-06-22 08:50:17 -0600

    Start FileManager

--- Bedrock/FileManager.swift
+++ Bedrock/FileManager.swift
@@ -30,4 +30,35 @@ struct FileManager {
 
 		return (true, st.st_mode == S_IFDIR)
 	}
+
+	static func changeDirectory(path: String) -> Bool {
+		let p = path.substringToIndex(advance(path.startIndex, Int(PATH_MAX))).fileSystemRepresentation()
+		return (chdir(p) == 0)
+	}
+
+	static func contentsOfDirectory(path: String) -> [String] {
+		let p = path.substringToIndex(advance(path.startIndex, Int(PATH_MAX))).fileSystemRepresentation()
+
+		let dir : UnsafeMutablePointer<DIR> = opendir(p);
+		let name_max = pathconf(p, _PC_NAME_MAX)
+		let buf : UnsafeMutablePointer<dirent> = UnsafeMutablePointer<dirent>.alloc(name_max + 1)
+		var de : UnsafeMutablePointer<dirent> = nil
+
+		// We ain't takin' none of that POSIX crap
+		if dir == nil || name_max == 0 || buf == nil {
+			return []
+		}
+
+		var res : [String] = []
+		while readdir_r(dir, buf, &de) == 0 && de != nil {
+			if let name = withUnsafePointer(&de.memory.d_name, {
+				return String.fromCString(UnsafePointer($0))
+			}) {
+				res.append(name)
+			}
+		}
+
+		closedir(dir)
+		return res
+	}
 }
